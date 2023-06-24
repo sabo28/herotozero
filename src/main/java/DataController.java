6f9cc67ae8ba3165b87686601467854ed8bcd975
sabo28 <labeeb.malik318@gmail.com>
2023-06-24 14:06:53 +0200

    improve front-end: -beautiful container with user friendly animation -invalid country image -use js for eventListener
    DataController.java:
    -no need for error message from DataController

--- src/main/java/DataController.java
+++ src/main/java/DataController.java
@@ -1,13 +1,11 @@
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
-import java.util.Objects;
 
 @Named
 @RequestScoped
 public class DataController {
     private final DataService dataService = new DataService();
     public String getEmissionData(String country) throws ClassNotFoundException {
-        String emissionData = dataService.fetch(country);
-        return (!Objects.equals(emissionData, "")) ? emissionData : "Emissionswert nicht gefunden";
+        return dataService.fetch(country);
     }
 }
